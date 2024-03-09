688bf4bcf62787e8eda3adcbf2b4bde576144a27
Animesh Singh <87444276+Animesh0764@users.noreply.github.com>
2024-03-09 10:30:19 +0530

    logo added

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import "../assets/css/Navbar.css";
 import { Link } from 'react-router-dom';
+import Logo from "../assets/images/logoapp.png";
 
 const Navbar = () => {
     return (
         <div className="navbar">
+            
             <ul>
-                <Link to="/" className='hyperlinks'><li>Home</li></Link>
-                <li>Public Info</li>
-                <li>Complaints</li>
+                <Link to="/" className='hyperlinks'><img src={Logo} alt="" srcset="" /></Link>
+                <Link to="/public-info" className='hyperlinks'><li>Public Info</li></Link>
+                <Link to="/complaints" className='hyperlinks'><li>Complaints</li></Link>
                 <Link to="/auth" className='hyperlinks'><li>Sign up</li></Link>
             </ul>
         </div>
